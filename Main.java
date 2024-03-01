1f1460c0cddafcd803be3f2e119992239af697df
Keerthika246 <147402553+Keerthika246@users.noreply.github.com>
2024-03-01 09:24:34 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,34 @@
+public class Main{
+  public static int removeDuplicates(int a[],int n)
+  {
+	if(n == 0 || n == 1){
+	   return n;
+	}
+	
+	int j = 0;
+
+	for(int i = 0;i < n - 1;i++){
+	    if(a[i] != a[i+1]){
+		a[j++] = a[i];
+	    }
+	}
+	
+	a[j++] = a[n - 1];
+	return j;
+	}
+
+	public static void main(String[] args)
+	{
+		int a[] ={1,2,1,1,3,2};
+
+		int n = a.length;
+		
+		int j=0;
+
+		j = removeDuplicates(a,n);
+
+		for(int i = 0;i<j;i++)
+			System.out.print(a[i] +" ");
+
+	}
+  }	
