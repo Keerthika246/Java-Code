084147ef9819ea9c648802b3f4180145899aa9af
Keerthika246 <147402553+Keerthika246@users.noreply.github.com>
2024-03-01 10:08:57 +0530

    Add files via upload

---
+++ PrintAsciiValue.java
@@ -0,0 +1,13 @@
+public class PrintAsciiValue{
+public static void main(String[] args){
+
+	char c1 ='a';
+	char c2 ='k';
+
+	int asciiValue1 = c1;
+	int asciiValue2 = c2;
+
+		System.out.println("The Ascii Value of " + " is: "+asciiValue1);
+		System.out.println("The Ascii Value of " + " is: "+asciiValue2);
+  }
+}
