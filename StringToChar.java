2a76a6e7d1bc33777565cd56c072756555cb2f16
Keerthika246 <147402553+Keerthika246@users.noreply.github.com>
2024-03-03 10:59:47 +0530

    Add files via upload

---
+++ StringToChar.java
@@ -0,0 +1,10 @@
+public class StringToChar
+{  
+public static void main(String args[])
+{  
+String s="Hello";  
+char c=s.charAt(0);
+//returns  0 index value H  
+System.out.println("First character is: "+c);  
+}
+}  
