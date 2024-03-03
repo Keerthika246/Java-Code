952a8e06e5b589efe710525b65c2e63e74636e4f
Keerthika246 <147402553+Keerthika246@users.noreply.github.com>
2024-03-03 11:41:03 +0530

    Add files via upload

---
+++ StringToInt.java
@@ -0,0 +1,10 @@
+public class StringToInt
+{  
+public static void main(String args[])
+{  
+String s="200";  
+int i=Integer.parseInt(s);  
+	System.out.println(s+100);
+	System.out.println(i+100);
+}
+}  
