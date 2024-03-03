c99bf0cc57035dfd8b79f0dd1607119af812e4fa
Keerthika246 <147402553+Keerthika246@users.noreply.github.com>
2024-03-03 11:15:46 +0530

    Add files via upload

---
+++ IntToString.java
@@ -0,0 +1,10 @@
+public class IntToString
+{  
+public static void main(String args[])
+{  
+int i=500;  
+String s = Integer.toString(i);  
+System.out.println(i+1000);
+System.out.println(s+1000); 
+}
+}  
