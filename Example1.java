a7e33fe54dd0ea6e2fec33f2f61bbfbcc24838b8
Keerthika246 <147402553+Keerthika246@users.noreply.github.com>
2024-03-04 21:40:03 +0530

    Add files via upload

---
+++ Example1.java
@@ -0,0 +1,11 @@
+public class Example1 {
+     public static void main(String[] args) {
+  	  int a = 125;
+	  int b = 24;
+	  System.out.println(a+b);
+	  System.out.println(a-b);
+	  System.out.println(a*b);
+ 	  System.out.println(a/b);
+ 	  System.out.println(a%b);
+ }
+} 
