3e71dc56712992031ac0a39f4482cb5af40c97b6
Keerthika246 <147402553+Keerthika246@users.noreply.github.com>
2024-08-18 22:44:09 +0530

    Add files via upload

---
+++ Basic.java
@@ -0,0 +1,27 @@
+import java.io.*;
+class Basic{
+static byte b;
+static int i;
+static long l;
+static short s;
+static char c;
+static Boolean bool;
+static float f;
+static String str;
+static Object o;
+static double d;
+static int[] Arr;
+public static void main(String[] args){
+System.out.println("byte value "+b);
+System.out.println("int value "+i);
+System.out.println("long value "+l);
+System.out.println("short value "+s);
+System.out.println("char value "+c);
+System.out.println("boolean value "+bool);
+System.out.println("float value "+f);
+System.out.println("string value "+str);
+System.out.println("object value "+o);
+System.out.println("double value "+d);
+System.out.println("Array value "+Arr);
+}
+}
